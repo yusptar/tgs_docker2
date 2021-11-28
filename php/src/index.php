faef557a325dbd1727d95b2c80c1dc194ed52870
yusptar <yusupvatar@gmail.com>
2021-11-28 10:53:18 +0700

    first commit

---
+++ php/src/index.php
@@ -0,0 +1,39 @@
+
+<?php
+echo "Simple Docker PHP & MySQL";
+
+// The MySQL service named in the docker-compose.yml.
+$host = '172.20.0.2';
+
+// Database use name
+$user = 'root';
+
+//database user password
+$pass = 'admin';
+
+// database name
+$mydatabase = 'php_user';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully! <br>";
+}
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
+?>
